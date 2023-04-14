4296e37888ba9e5f1571f466d16ed5ded6239256
sinat-jiang <jiangbiao0903@outlook.com>
2023-04-15 00:29:00 +0800

    consistence

--- proxy_manager.py
+++ proxy_manager.py
@@ -22,29 +22,22 @@ class ProxyManager:
     def get_proxy_num(self):
         return len(self.proxies)
 
-    def add_proxy(self, ip, port):
-        proxy_str = "{}:{}".format(ip, port)
+    def add_proxy(self, proxy):
+        proxy_str = "{}:{}".format(proxy['ip'], proxy['port'])
         if proxy_str not in self.proxies_set:
             self.proxies_set.add(proxy_str)
-            self.proxies.append({
-                'ip': ip,
-                'port': port
-            })
+            self.proxies.append(proxy)
 
     def add_proxies(self, proxies):
         for proxy in tqdm(proxies):
             proxy_str = "{}:{}".format(proxy['ip'], proxy['port'])
             if proxy_str not in self.proxies_set:
                 self.proxies_set.add(proxy_str)
-                self.proxies.append({
-                    'ip': proxy['ip'],
-                    'port': proxy['port']
-                })
+                self.proxies.append(proxy)
 
     def get_useful_proxy(self):
         for i in tqdm(range(len(self.proxies))):
-            index = random.randint(0, len(self.proxies) - 1)
-            proxy = self.proxies[index]
+            proxy = self.get_proxy()
             if check_proxy_900cha(proxy['ip'], proxy['port']):
                 return proxy
         print('无可用 proxy ~')
