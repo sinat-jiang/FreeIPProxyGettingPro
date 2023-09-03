b54e6ccd986d2733ea46ec7676eec32751bc484d
Jmouse <jiangbiao0903@outlook.com>
2023-09-03 14:05:00 +0800

    Update chain_of_spiders_cfg.py

--- chain_of_spiders_cfg.py
+++ chain_of_spiders_cfg.py
@@ -3,13 +3,8 @@
 # 自定义爬虫链
 
 from ProxiesSpider import (
-    eightnine_proxy_spider,
-    kaixin_proxy_spider,
     kuai_proxy_spider,
-    proxyhub_proxy_spider,
     seo_proxy_spider,
-    sixsix_proxy_spider,
-    yun_proxy_spider,
     zdaye_proxy_spider,
     stock_proxy_spider
 )
@@ -17,12 +12,7 @@ from ProxiesSpider import (
 
 proxy_spiders = [
     stock_proxy_spider.SpiderStock,
-    eightnine_proxy_spider.Spider89,
-    kaixin_proxy_spider.SpiderKaixin,
     kuai_proxy_spider.SpiderKuai,
-    proxyhub_proxy_spider.SpiderPH,
     seo_proxy_spider.SpiderSeo,
-    sixsix_proxy_spider.Spider66,
-    yun_proxy_spider.SpiderYun,
     zdaye_proxy_spider.SpiderZdaye
 ]
